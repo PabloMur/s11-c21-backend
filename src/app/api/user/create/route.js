57a4462ddbdc29e9d663ddbc2fcfba69833f1e67
PabloMur <polillomurillo@gmail.com>
2023-10-11 19:47:05 -0300

    asd

--- src/app/api/user/create/route.js
+++ src/app/api/user/create/route.js
@@ -1,19 +1,22 @@
 import { NextResponse } from "next/server";
 import { UserModel } from "@/models/User";
-import NextCors from "nextjs-cors";
 
-export async function POST(request, res) {
+export async function POST(request) {
   try {
-    await NextCors(request, res, {
-      // Options
-      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-      origin: "*",
-      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    });
     const body = await request.json();
     const { name, email } = body;
     const userTest = await UserModel.createUser(email, name);
-    return NextResponse.json({ userTest }, { status: 200 });
+    return NextResponse.json(
+      { userTest },
+      {
+        status: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    );
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error }, { status: 400 });
